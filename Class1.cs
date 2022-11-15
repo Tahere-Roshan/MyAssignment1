3bcc6f7bac2f56f5ba68d0b841a982ddc1a1c50d
Tahere <103662051+Tahere-Roshan@users.noreply.github.com>
2022-11-15 23:01:46 +0100

    Try

--- Class1.cs
+++ Class1.cs
@@ -9,10 +9,11 @@ using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
-
+using System.Runtime.CompilerServices;
 
 namespace Algorithm
 {
+   
     public class Myclass
     {
         //Part D
@@ -30,7 +31,7 @@ namespace Algorithm
         }
 
         //Randomize Method
-        public static int[] Randmize(int[] myArray)
+        public int[] Randmize(int[] myArray)
         {
             Random random = new();
             for (int i = 0; i < myArray.Length; i++)
@@ -42,7 +43,7 @@ namespace Algorithm
         }
 
         //Prepare Method
-        public static int[] Prepare(int Arraysize)
+        public int[] Prepare(int Arraysize)
         {
             int[] myArray = new int[Arraysize];
             return Randmize(myArray);
@@ -50,24 +51,32 @@ namespace Algorithm
         }
 
 
-        //Insertion Sort Method-https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-6.php
-        public static void InstertionSort(int[] myArray)
+
+       
+        //Insertion Sort Method-https://www.youtube.com/watch?v=yCxV0kBpA6M
+        
+        public void InstertionSort(int[] myArray)
         {
-            for (int i = 0; i <= myArray.Length - 1; i++) {
-                for (int j = i + 1; j > 0; j--)
+            int j, temp;
+      
+            for (int i = 1; i < myArray.Length; i++) {
+                temp= myArray[i];
+                j= i - 1;
+                while(j>= 0 && myArray[j]>temp)
                 {
-                    if (myArray[j - 1] > myArray[j])
-                    {
-                        Swap(myArray, j - 1, j);
-                    }
+                    myArray[j+1] = myArray[j];
+                    j--;
                 }
+                myArray[j+1] = temp;
             }
-            Console.WriteLine("Insertion Sort:" + myArray);
-            Console.ReadLine();
+            for(int count=0; count<myArray.Length; count++)
+                Console.WriteLine(myArray[count]);
+            
+           
         }
 
         //Selection Sort Method-https://youtu.be/yCxV0kBpA6M
-        public static void SelectionSort(int[] myArray)
+        public void SelectionSort(int[] myArray)
         {
             for (int i = 0; i < myArray.Length - 1; i++)
             {
@@ -82,16 +91,17 @@ namespace Algorithm
                 }
 
             }
-            Console.WriteLine("Selection Sort:" + myArray);
-            Console.ReadLine();
+            for (int count = 0; count < myArray.Length; count++)
+                Console.WriteLine(myArray[count]);
+
         }
 
         //Bubble Sort Method-https://www.educba.com/bubble-sort-in-c-sharp/
-        public static void BubbleSort(int[] myArray)
+        public void BubbleSort(int[] myArray)
         {
-            for (int i = 0; i < myArray.Length - 1; i++)
+            for (int i = 0; i < myArray.Length; i++)
             {
-                for (int j = 0; j < myArray.Length - i - 1; j++)
+                for (int j = 0; j < myArray.Length - 1; j++)
                 {
                     if (myArray[j] > myArray[j + 1])
                     {
@@ -99,32 +109,35 @@ namespace Algorithm
                     }
                 }
             }
-            Console.WriteLine("Bubble Sort:" + myArray);
-            Console.ReadLine();
+            for (int count = 0; count < myArray.Length; count++)
+                Console.WriteLine(myArray[count]);
+
         }
          
        //Merg Sort Metho-https://www.c-sharpcorner.com/blogs/a-simple-merge-sort-implementation-c-sharp
-        public static int[] MergeSort(int[] myArray)
+        public int[] MergeSort(int[] myArray)
         {
             int[] left;
             int[] right;
+            int midPoint = myArray.Length / 2;
             //int[] result = new int[myArray.Length];
             if (myArray.Length <= 1)
                 return myArray;
-            int midPoint = myArray.Length / 2;
+            
             left = new int[midPoint];
             if (myArray.Length % 2 == 0)
                 right = new int[midPoint];
             else
                 right = new int[midPoint + 1];
-            for (int i = 0; i <= midPoint; i++)
+            for (int i = 0; i < midPoint; i++)
             {
                 left[i] = myArray[i];
             }
             int x = 0;
             for (int i = midPoint; i <= midPoint + 1; i++)
             {
-                right[x++] = myArray[i];
+                right[x] = myArray[i];
+                x++;
             }
             left = MergeSort(left);
             right = MergeSort(right);
@@ -135,7 +148,7 @@ namespace Algorithm
                
 
        //Quick Sort Method-https://code-maze.com/csharp-quicksort-algorithm/
-        public static int[] QuickSort(int[] myArray)
+        public void QuickSort(int[] myArray)
         {
             int leftIndex = 0;
             int rightIndex = myArray.Length-1;
@@ -161,12 +174,13 @@ namespace Algorithm
             {
                 QuickSort(myArray);
             }
-            return myArray;
+            for (int count = 0; count < myArray.Length; count++)
+                Console.WriteLine(myArray[count]);
         }
            
 
        //Merge Method (useful for sort method)-https://www.c-sharpcorner.com/blogs/a-simple-merge-sort-implementation-c-sharp
-            static int[] Merge(int[] right, int[] left)
+            public int[] Merge(int[] right, int[] left)
                 {
                     int resultLength = left.Length + right.Length;
                     int[] result = new int[resultLength];
@@ -205,16 +219,27 @@ namespace Algorithm
                 }
 
         //Method "SortByLamba"-https://www.dotnetperls.com/sort-list-https://www.youtube.com/watch?v=F6iHzYLh2cA
-        public static void SortByLamba(int[] myArray)
+        //public void SortByLamba(int[] myArray, int x, int y)
+        //{
+        //    Array.Sort(myArray, (x, y)) => x.CompareTo(y));
+        //    Console.WriteLine(myArray);
+        //}
+        public void SortByLamba(int[] myArray)
         {
-            int[] LambaArray= new int[myArray.Length];
-            LambaArray= myArray.OrderBy(x => x).ToArray();
-            Console.WriteLine("Sort by Lamba:" + myArray);
-            Console.ReadLine();
+            //int[] LambaArray = new int[myArray.Length];
+            //LambaArray = myArray.OrderBy(x => x).ToArray();
+            ////Console.WriteLine("Sort by Lamba:" + myArray);
+            //for (int count = 0; count < myArray.Length; count++)
+            //    Console.WriteLine(myArray[count]);
+            Array.Sort(myArray, (x, y) => x.CompareTo(y));
+            for (int count = 0; count < myArray.Length; count++)
+                Console.WriteLine(myArray[count]);
+
         }
 
+
         //Method "DisplayRunningTime"
-        public static void DisplayRunngTime1(int[] myArray, SortedDelegate1 sorteddelgate1)
+        public void DisplayRunngTime1(int[] myArray, SortedDelegate1 sorteddelgate1)
         {
             Stopwatch myStopwatch = new();
             myStopwatch.Start();
@@ -242,7 +267,7 @@ namespace Algorithm
         }
 
         //Linear Search Method-https://www.geeksforgeeks.org/binary-search/?ref=lbp
-        public static int BinarySearch(int[] myArray, int item)
+        public int BinarySearch(int[] myArray, int item)
         {
             MergeSort(myArray);//which sort is the best?
             int low = 0, high = myArray.Length - 1;
@@ -260,7 +285,7 @@ namespace Algorithm
         }
 
         //Method "DisplayRunningTime"
-        public static void DisplayRunngTime2(int[] myArray, int item, SortedDelegate2 sorteddelgate2)
+        public void DisplayRunngTime2(int[] myArray, int item, SortedDelegate2 sorteddelgate2)
         {
             Stopwatch myStopwatch = new();
             myStopwatch.Start();
